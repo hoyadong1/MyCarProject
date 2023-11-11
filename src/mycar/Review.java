774dfee181c1636bca985c1b5ec2d43701cf6579
heung115 <heung115@naver.com>
2023-11-12 00:38:20 +0900

    :heavy_plus_sign: 리뷰 객체에서 리뷰를 남긴 옵션객체를 가지도록 수정

--- src/mycar/Review.java
+++ src/mycar/Review.java
@@ -6,9 +6,12 @@ public class Review implements Manageable {
 
     int rating = 0;
     String reviewComment = null;
+    Option option = null;
 
+    //input txt => 옵션이름 별점 리뷰내용
     @Override
     public void read(Scanner scan) {
+        option = MyCarProgram.findOption(scan.next());
         rating = scan.nextInt();
         reviewComment = scan.next();
     }
