e4fb2a39734ffef548d25cf42b03b6fab14e7368
geonu0916 <138549747+geonu0916@users.noreply.github.com>
2023-11-12 21:49:47 +0900

    Revert "Feature/find all"

--- src/mycar/Option.java
+++ src/mycar/Option.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class Option implements Manageable{
 	String name;
 	int price;
+	int fixPrice;
 
 	@Override
 	public void read(Scanner scan) {
 		name = scan.next();
 		price = scan.nextInt();
+		fixPrice = scan.nextInt();
 	}
 
 	@Override
 	public void print() {
-		System.out.printf("%s %d %d\n", name,price);
+		System.out.printf("%s %d %d\n", name,price,fixPrice);
 	}
 
 	@Override
