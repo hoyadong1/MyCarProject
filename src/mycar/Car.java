c1585943e912a3809a46580b7ba89f93ed9a1ada
heung115 <heung115@naver.com>
2023-11-21 16:53:19 +0900

    :sparkles: getCarName, getCarPrice 추가

--- src/mycar/Car.java
+++ src/mycar/Car.java
@@ -1,9 +1,12 @@
 package mycar;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 
 public class Car implements Manageable {
+
     String code;
     String name;
     int price;
@@ -26,8 +29,9 @@ public class Car implements Manageable {
 
         while (true) {
             String option = scan.next();
-            if (option.equals("0"))
+            if (option.equals("0")) {
                 break;
+            }
             Option o = MyCarProgram.findOption(option);
             optionList.add(o);
         }
@@ -36,7 +40,7 @@ public class Car implements Manageable {
     @Override
     public void print() {
         System.out.printf("%s %s %d %d %s %d %d\n\t\t", code, name, price, year, fuel,
-                fuelEfficiency, power);
+            fuelEfficiency, power);
         for (Option o : optionList) {
             System.out.print(o.name + " ");
         }
@@ -45,85 +49,109 @@ public class Car implements Manageable {
 
     @Override
     public boolean matches(String kwd) {
-        if (kwd.equals(code))
+        if (kwd.equals(code)) {
             return true;
-        if (kwd.equals(name))
+        }
+        if (kwd.equals(name)) {
             return true;
+        }
         return false;
     }
 
     public boolean isRange(CarRange c) {
-        if (price < c.startPrice || price > c.endPrice)
+        if (price < c.startPrice || price > c.endPrice) {
             return false;
-        if (year < c.startYear || year > c.endYear)
+        }
+        if (year < c.startYear || year > c.endYear) {
             return false;
+        }
         int i = 0;
         for (String compFuel : c.fuelList) {
-            if (compFuel.equals(fuel))
+            if (compFuel.equals(fuel)) {
                 break;
+            }
             i++;
         }
-        if (i == c.fuelList.size())
+        if (i == c.fuelList.size()) {
             return false;
-        if (fuelEfficiency < c.startFuelEffi || fuelEfficiency > c.endFuelEffi)
+        }
+        if (fuelEfficiency < c.startFuelEffi || fuelEfficiency > c.endFuelEffi) {
             return false;
-        if (power < c.startPower || power > c.endPower)
+        }
+        if (power < c.startPower || power > c.endPower) {
             return false;
+        }
         return true;
     }
 
     public int insertSelf(int row) {
         if (row == 0) {
-            Start1.data = new Object[][] {{code, name, price, year, fuel, fuelEfficiency, power},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},
-                    {null, null, null, null, null, null, null},};
+            Start1.data = new Object[][]{{code, name, price, year, fuel, fuelEfficiency, power},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},};
         } else {
             for (int i = 0; i < 7; i++) {
-                if (i == 0)
+                if (i == 0) {
                     Start1.data[row][i] = code;
-                if (i == 1)
+                }
+                if (i == 1) {
                     Start1.data[row][i] = name;
-                if (i == 2)
+                }
+                if (i == 2) {
                     Start1.data[row][i] = price;
-                if (i == 3)
+                }
+                if (i == 3) {
                     Start1.data[row][i] = year;
-                if (i == 4)
+                }
+                if (i == 4) {
                     Start1.data[row][i] = fuel;
-                if (i == 5)
+                }
+                if (i == 5) {
                     Start1.data[row][i] = fuelEfficiency;
-                if (i == 6)
+                }
+                if (i == 6) {
                     Start1.data[row][i] = power;
+                }
             }
         }
         return (row + 1);
     }
-    public List<String> getOptionListName(){
+
+    public List<String> getOptionListName() {
         ArrayList<String> optionListName = new ArrayList<>();
-        for(Option option : optionList){
+        for (Option option : optionList) {
             optionListName.add(option.name);
         }
         return optionListName;
+    }
+
+    public String getCarName() {
+        return name;
+    }
+
+    public int getCarPrice() {
+        return price;
     }
 }
