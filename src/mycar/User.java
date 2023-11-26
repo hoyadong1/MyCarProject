bb188700b083293e0ecc23e1aa052f7ee6250e3c
heung115 <heung115@naver.com>
2023-11-26 13:43:55 +0900

    :sparkles: user 싱글톤으로 변경

--- src/mycar/User.java
+++ src/mycar/User.java
@@ -4,9 +4,21 @@ import java.util.ArrayList;
 
 public class User {
 
+    private static User instance = null;
     ArrayList<Car> basket = new ArrayList<>();
 
     public void addBasket(Car car) {
         basket.add(car);
+    }
+
+    public static User getInstance() {
+        if (instance == null) {
+            instance = new User();
+        }
+        return instance;
+    }
+
+    private User() {
+        
     }
 }
