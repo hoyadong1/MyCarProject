1e731f4aac7f9a96203d1523b140b50a51c02698
Lenovo <Lenovo@DESKTOP-TSPQ9AI.mshome.net>
2023-12-04 00:59:37 +0900

    Merge branch 'branch1' of https://github.com/hoyadong1/MyCarProject.git into branch1

--- src/mycar/CarListPanel.java
+++ src/mycar/CarListPanel.java
@@ -142,8 +142,4 @@ public class CarListPanel extends JPanel {
         side.setIcon(sideImage);
         back.setIcon(backImage);
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> branch 'branch1' of https://github.com/hoyadong1/MyCarProject.git
